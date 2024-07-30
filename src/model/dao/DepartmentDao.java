70e17a744f1cfe818d6f8dbb912c9415e096dbc3
Abc Bolinhas <abc@bolinhas.com>
2024-07-30 15:42:57 -0300

    Criação de interfaces DAO

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepartmentDao {
+	public void insert(Department obj);
+	public void update(Department obj);
+	public void deleteById(Integer id);
+	public Department findById(Integer id);
+	public List<Department> findAll();
+}
