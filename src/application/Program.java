57fec66ac5b475c0a05e4c1e83b54b7a15271f29
Abc Bolinhas <abc@bolinhas.com>
2024-07-30 15:57:14 -0300

    Criando DaoFactory e sellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,8 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		Department d1 = new Department(1, "vendas");
-		System.out.println(d1);
 		Seller s1 = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, d1);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(d1);
+		
 		System.out.println(s1);
 	}
 
