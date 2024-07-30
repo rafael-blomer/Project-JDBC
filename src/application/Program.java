cfa6d1c6c4b05e56b24e3e2153011e7edbf32c54
Abc Bolinhas <abc@bolinhas.com>
2024-07-30 16:30:32 -0300

    implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department d1 = new Department(1, "vendas");
-		Seller s1 = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, d1);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(d1);
+		Seller seller = sellerDao.findById(1);
 		
-		System.out.println(s1);
+		System.out.println(seller);
 	}
 
 }
